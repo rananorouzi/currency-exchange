adb50a09c6bce362fb0d01dbb865f99b00985fe6
Rana <rn.norouzi@gmail.com>
2024-04-11 22:57:50 +0300

    Update App.tsx

--- currency-exchange/src/App.tsx
+++ currency-exchange/src/App.tsx
@@ -18,7 +18,6 @@ export default function App() {
   return (
     <div className="block max-w-sm rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
       <form>
-        {/* <!--E-mail input--> */}
         <TEInput
           type="currency"
           label="Currency"
